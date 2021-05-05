d103d6d184c0a669fec799f863a34bf3b4fe6e13
Tetiana <ttobin@protonmail.ch>
2021-05-05 13:50:14 +0100

    Fixed item deselect price calculation

--- src/PizzaReducer.js
+++ src/PizzaReducer.js
@@ -24,29 +24,83 @@ export default function reducer(state, action) {
     case 'SELECT_PIZZA_CHEESE_MOZARELLA':
       return { ...state, cheese: [...state.cheese, 'mozarella'] };
 
+    case 'DESELECT_PIZZA_CHEESE_MOZARELLA':
+      return {
+        ...state,
+        cheese: state.cheese.filter((el) => el !== 'mozarella'),
+      };
+
     case 'SELECT_PIZZA_CHEESE_CHEDDAR':
       return { ...state, cheese: [...state.cheese, 'cheddar'] };
+
+    case 'DESELECT_PIZZA_CHEESE_CHEDDAR':
+      return {
+        ...state,
+        cheese: state.cheese.filter((el) => el !== 'cheddar'),
+      };
 
     case 'SELECT_PIZZA_CHEESE_DOR_BLUE':
       return { ...state, cheese: [...state.cheese, 'dor blue'] };
 
+    case 'DESELECT_PIZZA_CHEESE_DOR_BLUE':
+      return {
+        ...state,
+        cheese: state.cheese.filter((el) => el !== 'dor blue'),
+      };
+
     case 'SELECT_PIZZA_VEG_TOMATO':
       return { ...state, veg: [...state.veg, 'tomato'] };
+
+    case 'DESELECT_PIZZA_VEG_TOMATO':
+      return {
+        ...state,
+        veg: state.veg.filter((el) => el !== 'tomato'),
+      };
 
     case 'SELECT_PIZZA_VEG_MUSHROOM':
       return { ...state, veg: [...state.veg, 'mushroom'] };
 
+    case 'DESELECT_PIZZA_VEG_MUSHROOM':
+      return {
+        ...state,
+        veg: state.veg.filter((el) => el !== 'mushroom'),
+      };
+
     case 'SELECT_PIZZA_VEG_PEPPER':
       return { ...state, veg: [...state.veg, 'pepper'] };
+
+    case 'DESELECT_PIZZA_VEG_PEPPER':
+      return {
+        ...state,
+        veg: state.veg.filter((el) => el !== 'pepper'),
+      };
 
     case 'SELECT_PIZZA_MEAT_BACON':
       return { ...state, meat: [...state.meat, 'bacon'] };
 
+    case 'DESELECT_PIZZA_MEAT_BACON':
+      return {
+        ...state,
+        meat: state.meat.filter((el) => el !== 'bacon'),
+      };
+
     case 'SELECT_PIZZA_MEAT_PEPPERONI':
       return { ...state, meat: [...state.meat, 'pepperoni'] };
 
+    case 'DESELECT_PIZZA_MEAT_PEPPERONI':
+      return {
+        ...state,
+        meat: state.meat.filter((el) => el !== 'pepperoni'),
+      };
+
     case 'SELECT_PIZZA_MEAT_HAM':
       return { ...state, meat: [...state.meat, 'ham'] };
+
+    case 'DESELECT_PIZZA_MEAT_HAM':
+      return {
+        ...state,
+        meat: state.meat.filter((el) => el !== 'ham'),
+      };
 
     default:
       return;
