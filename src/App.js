c7344b6093e206aeab8eb6a96324ec651dbeadbb
Tetiana <ttobin@protonmail.ch>
2021-05-09 15:07:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ const App = () => {
       <h1>Best Pizza in Town</h1>
       <h2>Staging</h2>
       <PizzaComponent />
-      <button onClick={methodDoesNotExist}>Break the world</button>;
+      <button
+        onClick={() => {
+          throw new Error('Something went wrong');
+        }}
+      >
+        Break the world
+      </button>
+      ;
     </div>
   );
 };
