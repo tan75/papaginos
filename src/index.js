67fe6c2565b74e815a01ab849869aeb88fa8a776
Tetiana <ttobin@protonmail.ch>
2021-05-10 22:11:16 +0100

    Add Sentry Integration

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { Integrations } from '@sentry/tracing';
 Sentry.init({
   dsn:
     'https://1d6c46ab9e1647f9a970888a5ef25235@o635742.ingest.sentry.io/5755850',
+  release: process.env.REACT_APP_SENTRY_RELEASE,
   integrations: [new Integrations.BrowserTracing()],
 
   // Set tracesSampleRate to 1.0 to capture 100%
